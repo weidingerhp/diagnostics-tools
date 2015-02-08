4b4626ac7865570a5c09ecd206725ad5e487cda7
Sebastian <lowleveldesign@gmail.com>
2015-02-08 19:21:38 +0100

    Added WindowsEnvironmentHost to allow debugging Topshelf Windows services.

--- WinSvcDiagnostics/Topshelf.Diagnostics/HostConfiguratorExtensions.cs
+++ WinSvcDiagnostics/Topshelf.Diagnostics/HostConfiguratorExtensions.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf.Builders;
 using Topshelf.HostConfigurators;
+using Topshelf.Runtime;
 
 namespace Topshelf.Diagnostics
 {
     public static class HostConfiguratorExtensions
     {
+        [Obsolete("Please use the ApplyCommandLineWithDebuggerSupport")]
         public static void ApplyCommandLineWithInwrapSupport(this HostConfigurator hc) {
+            ApplyCommandLineWithDebuggerSupport(hc);
+        }
+
+        public static void ApplyCommandLineWithDebuggerSupport(this HostConfigurator hc) {
             var svcpath = Assembly.GetExecutingAssembly().Location;
 
             var cmdline = Environment.CommandLine;
@@ -25,5 +32,17 @@ namespace Topshelf.Diagnostics
                 hc.ApplyCommandLine(cmdline.Substring(ind));
             }
         }
+
+        public static void UseWindowsHostEnvironmentWithDebugSupport(this HostConfigurator hc) {
+            hc.UseEnvironmentBuilder(() => { return new WindowsHostEnvironmentBuilderWithDebugSupport(); });
+        }
     }
+
+    class WindowsHostEnvironmentBuilderWithDebugSupport : EnvironmentBuilder
+    {
+        public HostEnvironment Build() {
+            return new WindowsHostEnvironmentWithDebugSupport();
+        }
+    }
+
 }
---
+++ WinSvcDiagnostics/Topshelf.Diagnostics/WindowsHostEnvironmentWithDebugSupport.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf.Runtime;
+using Topshelf.Runtime.Windows;
+
+namespace Topshelf.Diagnostics
+{
+    class WindowsHostEnvironmentWithDebugSupport : HostEnvironment
+    {
+        private readonly WindowsHostEnvironment wrappedHost = new WindowsHostEnvironment();
+
+        public bool IsRunningAsAService {
+            get { return Process.GetCurrentProcess().SessionId == 0; }
+        }
+
+        public string CommandLine {
+            get { return wrappedHost.CommandLine; }
+        }
+
+        public Host CreateServiceHost(HostSettings settings, ServiceHandle serviceHandle) {
+            return wrappedHost.CreateServiceHost(settings, serviceHandle);
+        }
+
+        public void InstallService(InstallHostSettings settings, Action beforeInstall, Action afterInstall, Action beforeRollback, Action afterRollback) {
+            wrappedHost.InstallService(settings, beforeInstall, afterInstall, beforeRollback, afterRollback);
+        }
+
+        public bool IsAdministrator {
+            get { return wrappedHost.IsAdministrator; }
+        }
+
+        public bool IsServiceInstalled(string serviceName) {
+            return wrappedHost.IsServiceInstalled(serviceName);
+        }
+
+        public bool IsServiceStopped(string serviceName) {
+            return wrappedHost.IsServiceStopped(serviceName);
+        }
+
+        public bool RunAsAdministrator() {
+            return wrappedHost.RunAsAdministrator();
+        }
+
+        public void SendServiceCommand(string serviceName, int command) {
+            wrappedHost.SendServiceCommand(serviceName, command);
+        }
+
+        public void StartService(string serviceName, TimeSpan startTimeOut) {
+            wrappedHost.StartService(serviceName, startTimeOut);
+        }
+
+        public void StopService(string serviceName, TimeSpan stopTimeOut) {
+            wrappedHost.StopService(serviceName, stopTimeOut);
+        }
+
+        public void UninstallService(HostSettings settings, Action beforeUninstall, Action afterUninstall) {
+            wrappedHost.UninstallService(settings, beforeUninstall, afterUninstall);
+        }
+    }
+}
